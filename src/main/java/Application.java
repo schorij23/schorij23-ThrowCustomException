0508b9d455362dba0c9383621d751c723c6396d2
schorij23 <j_schori@aol.com>
2023-02-28 01:51:46 +0000

    Commit

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,6 +2,7 @@ public class Application {
     /**
      * This class contains a main method that allows you to manually test the CustomException lab.
      * No changes you make here affect the test case results. You must still run & pass the test cases.
+     * Changes
      * All implementation code should be written in the Lab class.
      *
      * The Lab.mustThrow method should throw the CustomException class.
--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,3 +1,4 @@
+import kotlin.jvm.Throws;
 
 public class Lab {
 
@@ -14,9 +15,13 @@ public class Lab {
      * This also means that you should NOT write a try/catch block in this method, as the tests are expecting to have
      * a method thrown to it. A try/catch block would handle the exception within the method. The tests, on the other
      * hand, are using a try/catch block to catch the exception thrown by this method.
+     * @throws CustomException
      */
-    public void throwCustomException(){
+    public void throwCustomException() throws CustomException{
+        throw new CustomException("FileNotFound");
 
     }
 
-}
+    }
+
+
